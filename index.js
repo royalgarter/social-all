370885d3768baaac9e8b902300f538686ef77927
royalgarter@gmail.com <royalgarter@gmail.com>
2017-10-03 13:40:16 +0700

    + logo, onOpenNewTab, Settings * layout

--- index.js
+++ index.js
@@ -4,6 +4,40 @@ var $ = function(sel) {
 	return document.querySelectorAll(sel);
 };
 
+var onNewWindow = function (event) {
+	// console.log("onNewWindow:", event.windowOpenDisposition);
+
+	switch (event.windowOpenDisposition) {
+		case 'ignore':
+			// Not sure what this is used by.  Default enum value, maybe.
+			console.log('event.windowOpenDisposition=ignore');
+		return;
+
+		case 'save_to_disk':
+			// Ctrl + S, maybe?  Not sure how to reproduce that.
+			console.log('event.windowOpenDisposition=save_to_disk');
+		return;
+
+		case 'current_tab':
+			console.log("event.windowOpenDisposition=current_tab");
+		return;
+
+		case 'new_background_tab':
+		case 'new_foreground_tab':
+			newWindow = open(event.targetUrl, '_blank');
+			if (event.windowOpenDisposition != 'new_background_tab') {
+				newWindow.focus();
+			}
+		break;
+
+		case 'new_window':
+		case 'new_popup':
+			newWindow = open(event.targetUrl, '_blank');
+			newWindow.focus();
+		break;
+	}
+}
+
 var init = function (urls, tabIdx) {
 	if (!urls) return;
 
@@ -17,7 +51,7 @@ var init = function (urls, tabIdx) {
 
 	$('#footer .tab-header')[0].appendChild(tabhead);
 	$('#footer .tab-header #tab-header-'+tabIdx)[0].addEventListener("click", function(){
-		console.log('click tabId', tabId);
+		// console.log('click tabId', tabId);
 
 		var activeTabs = $('.wv-tbl.active');
 		for (var i = 0; i < activeTabs.length; i++) {
@@ -25,14 +59,20 @@ var init = function (urls, tabIdx) {
 			activeTabs[i].className = activeTabs[i].className.replace(" active", "");
 		}
 
+		var activeTabHeader = $('.tab-header.active');
+		for (var i = 0; i < activeTabHeader.length; i++) {
+			activeTabHeader[i].className = activeTabHeader[i].className.replace(" active", "");
+		}
+
 		$('#'+tabId)[0].style.display = "";
 		$('#'+tabId)[0].className += " active";
+		$('#'+tabHeadId)[0].className += " active";
 	});
 
 	var tab = document.createElement('div');
 	tab.setAttribute("id", tabId);
 	tab.setAttribute("class", "wv-tbl");
-	$('body')[0].appendChild(tab);
+	$('body #tabs')[0].appendChild(tab);
 
 	var htmlTabTpl = $('#tpl-wv-cell')[0].outerHTML;
 
@@ -111,5 +151,14 @@ onload = function() {
 
         $('#tpl-wv-cell')[0].remove();
         $('#tab-header-0')[0].click();
+
+        var wvs = $('.webview-tab');
+		for (var i = 0; i < wvs.length; i++) {
+			wvs[i].addEventListener('newwindow', onNewWindow);
+		}
+
+		$('#footer #tab-header-setting')[0].addEventListener("click", function(){
+			$('#footer #txt-urls')[0].style.display = $('#footer #txt-urls')[0].style.display == 'none' ? '' : 'none';
+		});
     });
 }
--- main.js
+++ main.js
@@ -7,19 +7,5 @@ chrome.app.runtime.onLaunched.addListener(function() {
 	chrome.app.window.create('index.html', {
 		id: 'embedder',
 		state: "fullscreen",
-	}, function(win) {
-		win.contentWindow.onload = function() {
-			console.log('win.contentWindow.onload');
-			var webviews = win.contentWindow.document.querySelectorAll('webview');
-			for (var i = 0; i < webviews.length; i++) {
-				webviews[i].addEventListener('newwindow', function(e) {
-					console.log('newwindow');
-					e.preventDefault();
-					chrome.app.window.create(e.targetUrl, {
-						state: "fullscreen",
-					});
-				});
-			}
-		};
 	});
 });
