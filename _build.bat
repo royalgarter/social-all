e0a5af0d32878ff6cfa49369e520ec8590f2b73f
royalgarter@gmail.com <royalgarter@gmail.com>
2017-11-24 12:33:23 +0700

    finish build.bat for window

--- _build.bat
+++ _build.bat
@@ -3,8 +3,9 @@ if exist nwjs-v0.26.6-win-x64.zip (
 ) else (
     curl -O https://dl.nwjs.io/v0.26.6/nwjs-v0.26.6-win-x64.zip
 )
-
-7za e nwjs-v0.26.6-win-x64.zip .\nw
+rmdir /sq .\nw
+7za x nwjs-v0.26.6-win-x64.zip
+ren nwjs-v0.26.6-win-x64 nw
 7za a -tzip webview.nw *.js *.json *.html *.css ./assets ./lib
 copy /bY ".\nw\nw.exe"+webview.nw webview.exe
 move /Y webview.exe .\nw
